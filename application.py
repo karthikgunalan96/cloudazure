de5b61553613b71e2021bc3466977c8c12dca699
karthikgunalan96 <karthik.gunalan96@gmail.com>
2019-06-16 14:55:22 -0500

    Enter

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 """Cloud Foundry test"""
 from flask import Flask
 import os
+import sqlite3
 
 app = Flask(__name__)
 
@@ -9,7 +10,13 @@ port = int(os.getenv("PORT", 5000))
 
 @app.route('/')
 def hello_world():
-    return 'Hello azure!'
+    con = sqlite3.connect("quakes")
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    cur.execute("select count(*) from quakes")
+    rows = cur.fetchall()
+    return render_template('home.html',rows=rows)
+
 
 if __name__ == '__main__':
     app.run (port=port)
