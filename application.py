5a2164d0fab38b1b3c1d6135baa95446749a82b7
karthikgunalan96 <karthik.gunalan96@gmail.com>
2019-06-16 19:33:41 -0500

    Enter

--- application.py
+++ application.py
@@ -1,21 +1,46 @@
 """Cloud Foundry test"""
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import os
 import sqlite3
+import time
+import pyodbc
+from pymemcache.client import base
 
 app = Flask(__name__)
 
-print(os.getenv("PORT"))
+
 port = int(os.getenv("PORT", 5000))
 
 @app.route('/')
-def hello_world():
-    con = sqlite3.connect("quakes")
+def home():
+    
+    return render_template('home.html')
+
+@app.route('/magnitude', methods=['GET','POST'])
+def mag():
+    mag = str(request.form['mag'])
+    
+    # client = base.Client(('localhost', 11211))
+    # result = client.get(mag)
+    # if result is None:
+    start=time.time()
+    print("retrieved from database")
+    con = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server};Server=tcp:karthikgunalan.database.windows.net,1433;Database=assignment3;Uid=karthikgunalan@karthikgunalan;Pwd={Polo5590};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
     con.row_factory = sqlite3.Row
     cur = con.cursor()
-    cur.execute("select (count(*)) as count from all_month")
+    cur.execute("select * from all_month where mag > ?",(mag,))
     rows = cur.fetchall()
-    return render_template('home.html',rows=rows)
+    print(rows)
+    client.set(mag,rows)
+    end=time.time()
+    print('Time Taken')
+    print(end-start)
+    return render_template('magnitude1.html',rows=rows)
+    # else:
+    #     print("retrieved from memcache")
+        
+    #     return render_template('magnitude2.html',rows=result.sqlite3.Row)
+
 
 
 if __name__ == '__main__':
