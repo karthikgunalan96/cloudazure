8cdfa6eb0d1fc23b66cfbe218bd551d0fc660d02
karthikgunalan96 <karthik.gunalan96@gmail.com>
2019-06-19 23:17:27 -0500

    Enter

---
+++ static/index.js
@@ -0,0 +1,9 @@
+d3.select('h1').style('color','red');
+d3.selectAll('h2').style('color','red');
+d3.selectAll('p').style('color','blue');
+d3.select('p').style('color','blue');
+
+
+d3.selectAll('p').style('color','blue')
+.enter()
+
